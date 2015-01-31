f7a8350f2ac64fdb9906ea9aadb08378a4b5cc11
Steven <steven.github@gmail.com>
2015-01-30 17:55:06 -0800

    removed parallax scroll

--- animation.js
+++ animation.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+
+
   // hide #back-top first
   $("#back-to-top").hide();
 
-  $(function () {
+  // Back-to-top button scrolling animation javascript
+
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
         $('#back-to-top').fadeIn();
@@ -19,9 +22,10 @@ $(document).ready(function() {
       // This prevents the default action of the anchor tag to be called.
       //  Can be interchanged with event.preventDefault()
       return false;
-
     });
 
+
+    // Navbar scrolling animation javascript
     $("#navbar a").click(function(event) {
       stopScroll();
 
@@ -32,18 +36,18 @@ $(document).ready(function() {
       $('html,body').animate({scrollTop: jumpToTag.offset().top}, 800, continueScroll);
       return false;
     });
-  });
+
 });
 
 // Helper Functions
 // Taken from http://stackoverflow.com/a/18445654
 function stopScroll() {
   // if user scrolls while animation, cancel the animation
-  $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+  $("html, body").bind("scroll mousedown wheel keyup", function(){
     $('html, body').stop();
   });
 }
 
 function continueScroll() {
-  $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+  $("html, body").unbind("scroll mousedown wheel keyup");
 }
